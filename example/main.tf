72bb2ba61992c732a9fdbeb303b835a5c97efa1b
mstefaniak <Mikolaj.Stefaniak@eu.equinix.com>
2021-04-26 10:49:11 +0200

    Bumped up versions of module and provider

--- example/main.tf
+++ example/main.tf
@@ -5,7 +5,7 @@ provider "equinix" {
 
 module "vmware-sdwan" {
   source               = "equinix/vmware-sdwan/equinix"
-  version              = "1.0.0-beta"
+  version              = "1.0.0"
   name                 = "tf-vmware-sdwan"
   metro_code           = var.metro_code_primary
   platform             = "medium"
